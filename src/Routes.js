efbe517db730b5b7436f4c02cc5e086ab27be851
Nelsonmsk <nelsonmsk@gmail.com>
2024-07-19 16:18:14 +0200

    Add menu navigation for small screens

--- src/Routes.js
+++ src/Routes.js
@@ -8,21 +8,21 @@ import Profile from './views/Users/Profile';
 import EditProfile from './views/Users/EditProfile';
 import FindPeople from './views/Users/FindPeople';
 import NewsFeed from './views/Posts/NewsFeed';
-import {Menu} from './views/Core/Menu';
+import {MenuBar} from './views/Core/Menu';
 import Footer from './views/Core/Footer';
 import PrivateRoute from './views/Auth/PrivateRoute';
 
 const MainRouter = () => {
 	return(
 	 <div>
-		<Menu/>
+		<MenuBar/>
 		<Routes>
 			<Route exact path="/" element={<Home />}/>
-			<Route path="/users" element={<Users />}/>
 			<Route path="/signup" element={<Signup />}/>
 			<Route path="/signin" element={<Signin />}/>
+			<Route path="/users" element={<PrivateRoute path={"/users"} element={<Users />} /> } />
 			<Route path="/users/edit/:userId" element={<PrivateRoute path={"/users/edit/:userId"} element={<EditProfile />} /> } />
-			<Route path="/users/:userId" element={<Profile />} />
+			<Route path="/users/:userId" element={<PrivateRoute path={"/users/:userId"} element={<Profile />} /> } />
 			<Route path="/users/findpeople/:userId" element={<PrivateRoute path={"/users/findpeople/:userId"} element={<FindPeople />} /> } />
 			<Route path="/posts/feed/:userId" element={<PrivateRoute path={"/posts/feed/:userId"} element={<NewsFeed />} /> } />		
 		</Routes>
--- src/views/Core/Menu.js
+++ src/views/Core/Menu.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {Link, useLocation, useNavigate,} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles'
-import { AppBar, Toolbar, IconButton,Button } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Button, Menu, MenuItem } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
 import Typography from '@material-ui/core/Typography';
 
@@ -14,15 +15,121 @@ const useStyles = makeStyles(theme => ({
 	},
 	links: {
 		textDecoration: 'none !important'
+	},
+	toolbar_lg: {
+		visibility: 'visible',
+		display: 'flex',
+		[theme.breakpoints.down('sm')]: {
+			visibility: 'hidden',
+			display: 'none'
+		},
+	},
+	toolbar_sm: {
+		visibility: 'visible',
+		[theme.breakpoints.up('md')]: {
+			visibilty: 'hidden',
+			display: 'none'
+		},
+	},
+	menuButton: {
+		marginLeft: '45%',
+		marginRight: '-12px'
 	}
 }));
 
-export const Menu = withRouter(() => {
-const classes = useStyles();
-const navigate = useNavigate();
+export const MenuBar = withRouter(() => {
+	const classes = useStyles();
+	const navigate = useNavigate();
+	const [anchor, setAnchor] = useState(null);
+	const [screenWidth, setScreenWidth] = useState(0);
+
+	const closeMenu = () => setAnchor(null);
+
+	useEffect(()=>{
+		const handleResize = () => setScreenWidth(window.innerWidth);
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	},[]);
+
+	const IsActive = (path) =>{
+		let location = useLocation();
+		if (location.pathname === path){
+			return {color: '#ff4081'};
+		}else{
+			if(screenWidth && screenWidth > 992){
+				return {color: '#ffffff'};
+			}else if(screenWidth && screenWidth < 992){
+				return {color: '#3f4771'};
+			}else{
+				return {color: '#cccccc'};
+			}
+		}
+	};
+
+	const MenuItems = ({closeMenu }) => (
+		!auth.isAuthenticated() ? 
+		<Fragment>
+			<MenuItem onClick={closeMenu}>			
+				<Link to="/" >
+					<IconButton aria-label="Home" style={IsActive("/")}>
+						<HomeIcon/>
+					</IconButton>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+				<Link to="/signup" className={classes.links}>
+					<Button style={IsActive("/signup")}> Sign Up </Button>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+				<Link to="/signin" className={classes.links}>
+					<Button style={IsActive("/signin")}> Sign In </Button>
+				</Link> 
+			</MenuItem>
+		</Fragment>
+		: <Fragment>
+			<MenuItem onClick={closeMenu}>			
+				<Link to="/" >
+					<IconButton aria-label="Home" style={IsActive("/")}>
+						<HomeIcon/>
+					</IconButton>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+				<Link to={"/posts/feed/" + auth.isAuthenticated().user._id}  className={classes.links} >
+					<Button style={IsActive("/posts/feed/" + auth.isAuthenticated().user._id)}> NewsFeed </Button>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+				<Link to={"/users/findpeople/" + auth.isAuthenticated().user._id}  className={classes.links} >
+					<Button style={IsActive("/users/findpeople/" + auth.isAuthenticated().user._id)}> Find People </Button>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+				<Link to="/users" className={classes.links} >
+					<Button style={IsActive( "/users")}>Users</Button>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+			<Link to={"/users/" + auth.isAuthenticated().user._id}>
+					<Button style={IsActive("/users/" + auth.isAuthenticated().user._id
+					)}>
+						My Profile
+					</Button>
+				</Link>
+			</MenuItem>
+			<MenuItem onClick={closeMenu}>
+				<Button color="inherit"
+					onClick={() => { auth.clearJWT(() =>navigate('/signin') ); }}>
+						Sign out
+				</Button>
+			</MenuItem>
+		</Fragment>
+	);
+
 return(
 	<AppBar position="static">
-		<Toolbar>
+		<Toolbar className={classes.toolbar_lg}>
 			<Typography variant="h6" color="inherit" className={classes.title}>
 				Chattome
 			</Typography>
@@ -65,13 +172,19 @@ return(
 			</span>)
 			}
 		</Toolbar>
-</AppBar>)});
-	
-	function IsActive (path){
-		let location = useLocation();
-		if (location.pathname === path){
-			return {color: '#ff4081'};
-		}else{
-			return {color: '#ffffff'};
-		}
-	};
+		<Toolbar className={classes.toolbar_sm}>
+			<Typography variant="h6" color="inherit" className={classes.title}>
+				Chattome
+			</Typography>
+			<Menu anchorEl={anchor} open={Boolean(anchor)} 
+					onClose={closeMenu}>
+				<MenuItems closeMenu={closeMenu} />
+			</Menu>
+			<IconButton className={classes.menuButton} color={"inherit"} aria-label={"Menu"}
+					onClick={e =>setAnchor(e.currentTarget )}>
+				<MenuIcon />
+			</IconButton>
+		</Toolbar>
+	</AppBar>
+	)
+});
